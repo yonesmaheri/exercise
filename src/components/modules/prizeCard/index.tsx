214f03a6e8b8e4d34b46acda10c204a4685017c5
Yones Maheri <yonesmaheri80@gmail.com>
2024-08-29 10:10:52 +0330

    fix something

--- src/components/modules/prizeCard/index.tsx
+++ src/components/modules/prizeCard/index.tsx
@@ -12,38 +12,42 @@ function PrizeCard() {
         <div className="border-2 border-[#5F5F72] bg-[#262630] flex items-center justify-center rounded-xl w-[90px] h-[65px] lg:w-[103px] lg:h-[103px]">
           <Image src={"/logo-sample 4.png"} alt="logo" width={50} height={47} />
         </div>
-        <div className="bg-[#F0F0F0] font-bold w-full rounded-xl p-3 lg:hidden flex items-center">
+        <div className="bg-[#F0F0F0] text-sm font-bold h-full w-full rounded-xl p-3 lg:hidden flex items-center">
           خرید از فروشگاه زنجیره ای رفاه
         </div>
       </div>
 
       <div className="flex flex-col justify-between h-full w-full lg:w-[357px] ">
-        <div className="bg-[#F0F0F0] w-full hidden lg:block font-bold rounded-xl p-3">
+        <div className="bg-[#F0F0F0] w-full hidden lg:block font-bold text-base md:text-sm rounded-xl p-3">
           خرید از فروشگاه زنجیره ای رفاه
         </div>
         <div className="bg-[#565662] w-full flex items-center rounded-xl justify-between">
-          <div className=" text-white p-3 w-[50%] text-right font-bold">
+          <div className=" text-white text-sm lg:text-base p-3 w-[50%] text-right font-bold">
             کد تخفیف خرید:
           </div>
-          <div className=" bg-[#262630] font-bold py-3 px-6 text-white w-[50%] flex items-center justify-between rounded-xl text-center">
-            <span>FGA6799AGK</span>
-            <DocumentIcon />
+          <div className=" bg-[#262630] font-bold py-3 px-6 text-white w-[50%] flex items-center justify-center gap-4 rounded-xl text-center">
+            <span className="text-sm lg:text-base">FGA6799AGK</span>
+            <div className="w-6 h-6">
+              <DocumentIcon />
+            </div>
           </div>
         </div>
       </div>
 
       <div className="flex flex-col justify-between h-full gap-4 lg:gap-0 w-full lg:w-[285px]">
         <div className="bg-[#565662] w-full flex items-center rounded-xl justify-between">
-          <div className="font-bold text-white p-3 w-[40%] text-right">مبلغ تخفیف:</div>
-          <div className="font-bold bg-[#262630] py-3 px-6 text-white w-[60%] flex items-center justify-center rounded-xl text-center">
+          <div className="font-bold text-white p-3 w-[50%] lg:w-[40%] text-right text-sm lg:text-base">
+            مبلغ تخفیف:
+          </div>
+          <div className="font-bold text-sm lg:text-base bg-[#262630] py-3 px-6 text-white w-[50%] lg:w-[60%] flex items-center justify-center rounded-xl text-center">
             <span>550000 تومان</span>
           </div>
         </div>
         <div className="bg-[#565662] w-full flex items-center rounded-xl justify-between">
-          <div className="font-bold text-white p-3 w-[60%] text-right">
+          <div className="font-bold text-white p-3 w-[50%] lg:w-[60%] text-right text-sm lg:text-base">
             امتیاز مورد نیاز:
           </div>
-          <div className="font-bold bg-[#262630] py-3 px-6 text-white w-[40%] flex items-center justify-center rounded-xl text-center">
+          <div className="font-bold text-sm lg:text-base bg-[#262630] py-3 px-6 text-white w-[50%] lg:w-[40%] flex items-center justify-center rounded-xl text-center">
             <span>70 امتیاز</span>
           </div>
         </div>
